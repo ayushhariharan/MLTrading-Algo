0336e517c7a11fdc3729259d6f94b64ebceb3bae
ayush0624 <31373700+ayush0624@users.noreply.github.com>
2021-04-12 03:46:03 -0700

    Completed Data Fetching

--- algo.py
+++ algo.py
@@ -2,8 +2,13 @@ import bs4 as bs
 import pickle
 import requests
 import streamlit as st
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader.data as web
+import time 
 
-st.sidebar.title("Stock Market Prediction with ML")
+st.sidebar.title("Stock Market Prediction with Machine Learning")
 def save_tickers():
     resp = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
     soup = bs.BeautifulSoup(resp.text)
@@ -13,11 +18,57 @@ def save_tickers():
     for row in table.findAll('tr')[1:]: #iterates through the row of the table
         ticker = row.findAll('td')[0].text[:-1] #Finds the symbol for that specific row
         tickers.append(ticker) #Adds it to the ticker value
-    st.write(tickers)
 
     with open("tickers.pickle", 'wb') as f:
         pickle.dump(tickers, f)
     
     return tickers
 
+def fetch_data(num_count):
+    with open("tickers.pickle", 'rb') as f:
+        tickers = pickle.load(f)
+    if not os.path.exists('stock_details'):
+        os.makedirs('stock_details')
+    
+    start_date = dt.datetime(2010, 1, 1)
+    yesterday = dt.date.today() - dt.timedelta(days=1)
+    end_date = yesterday - dt.timedelta(days=yesterday.weekday())
+    count = 0
+
+    st.markdown('### Task Progress')
+    st.write('Fetching Data from Yahoo Finance for {} Stocks...'.format(num_count))
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for ticker in tickers:
+        if count == num_count:
+            break
+        count += 1
+        latest_iteration.text(f'Stock {ticker} | Iteration {count}')
+        bar.progress(count / num_count)
+
+        if not os.path.exists('stock_details/{}.csv'.format(ticker)):
+            try:
+                df = web.DataReader(ticker, 'yahoo', start_date, end_date)
+                df.to_csv('stock_details/{}.csv'.format(ticker))
+            except:
+                st.write("Stock {} does not exist".format(ticker))
+                continue    
+        else:
+            continue
+    
+    st.write('Completed Data Collection')
+
 save_tickers()
+count = st.sidebar.selectbox(
+            "How many Stocks to Consider?", [200, 300, 400, 500])
+
+if count is None:
+    count = 200
+
+if st.sidebar.button("Fetch Data", key="fetch"):
+    fetch_data(count)
+
+    if st.sidebar.button("Predict", key="predict"):
+        st.write("hello")
+
